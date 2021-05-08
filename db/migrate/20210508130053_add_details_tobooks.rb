7fc3f3b230b4df7c5fab85fcceb7d3a74f9802c5
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-08 23:46:50 +0900

    [add]user_idカラム

---
+++ db/migrate/20210508130053_add_details_tobooks.rb
@@ -0,0 +1,5 @@
+class AddDetailsTobooks < ActiveRecord::Migration[6.1]
+  def change
+    add
+  end
+end
---
+++ db/migrate/20210508130400_remove_details_tobooks.rb
@@ -0,0 +1,4 @@
+class RemoveDetailsTobooks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
