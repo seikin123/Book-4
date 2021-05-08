2ab717b0cea52bf561d4c8a86747e6cd4ff43d59
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-08 23:43:05 +0900

    投稿機能完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,32 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    
-  end
-
-  def new
-    @book = Book.new #空のインスタンスを生成
+    @booknew = Book.new
+    @user = current_user
   end
 
   def show
+    @booknew = Book.new
     @book = Book.find(params[:id])
+    @user = current_user
+    
   end
 
   def edit
   end
 
   def create
-    @book = Book.new(book_params) #データを新規登録するためのインスタンス生成
-    @book.save #データをデータベースに保存するためのsaveメソッド実行
+    @booknew = Book.new(book_params) #データを新規登録するためのインスタンス生成
+    @user = current_user
+    @booknew.user_id = current_user.id
+    if @booknew.save #データをデータベースに保存するためのsaveメソッド実行
+      redirect_to book_path(@booknew.id),notice: 'Book was succesfully created'
+    else
+      @books = Book.all
     redirect_to action: 'index' #トップ画面へリダイレクト
   end
+end
+
   
   private
     def book_params #ストロングパラメータ
